c8f8fea3a44146d60236162f3b818c299c8e1d96
Josue Costa <juncosta@gmail.com>
2024-10-25 22:13:02 -0300

    Adds admin course creation and some validations to it

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,7 +1,30 @@
 class Admin::CoursesController < Admin::BaseController
+
+  before_action :prepare_course, only: %i[new create]
+  
   def index
     @courses = Course.all
   end
 
   def new; end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to admin_courses_path, notice: 'Curso cadastrado com sucesso'
+    else
+      redirect_to new_admin_course_path, alert: @course.errors.full_messages.to_sentence
+    end
+  end
+
+  private 
+
+  def prepare_course
+    @course = Course.new
+  end
+
+  def course_params
+    params.require(:course).permit(:name, :description, :featured)
+  end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,9 @@
 class Course < ApplicationRecord
   has_many :enrollments
   has_many :students, through: :enrollments
+
+  validates :name, :description, :featured, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 10 }
+  validates :featured, numericality: { in: 1..10 }
 end
