3da3c4a0d1a94d0888429dd02639f89337bca196
Josue Costa <juncosta@gmail.com>
2024-10-26 22:26:32 -0300

    adds edit and update for course, moves form to partial

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CoursesController < Admin::BaseController
 
   before_action :prepare_course, only: %i[new create]
+  before_action :set_course, only: %i[edit update]
   
   def index
     @courses = Course.all
@@ -18,7 +19,17 @@ class Admin::CoursesController < Admin::BaseController
     end
   end
 
-  private 
+  def edit; end
+
+  def update
+    if @course.update(course_params)
+      redirect_to admin_courses_path, notice: 'Curso atualizado com sucesso'
+    else
+      redirect_to edit_admin_course_path, alert: @course.errors.full_messages.to_sentence
+    end
+  end
+
+  private
 
   def prepare_course
     @course = Course.new
@@ -26,5 +37,9 @@ class Admin::CoursesController < Admin::BaseController
 
   def course_params
     params.require(:course).permit(:name, :description, :featured)
+  end
+
+  def set_course
+    @course = Course.find(params[:id])
   end
 end
