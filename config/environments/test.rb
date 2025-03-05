fd5704d046a5048be7b0baa18886b491b6e48a94
Josue Costa <44443163+J-Costa@users.noreply.github.com>
2025-03-05 12:17:15 -0300

    Update rails, skip test, adds default scopes

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
 
+  config.active_support.to_time_preserves_timezone = :zone
+
   # Eager loading loads your entire application. When running a single test locally,
   # this is usually not necessary, and can slow down your test suite. However, it's
   # recommended that you enable it in continuous integration systems to ensure eager
@@ -64,4 +66,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # freeze classes to solve mapping error for devise
+  config.cache_classes = true
 end
