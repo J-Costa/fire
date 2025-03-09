4b36c38babc6439ce1793bf2f7b7a351490a7bb2
Josue Costa <44443163+J-Costa@users.noreply.github.com>
2025-03-08 22:21:02 -0300

    add tests for contacts controller (#44)
    * add tests for contacts controller
    
    * Adds coverage action
    
    * Adds octocov yml
    
    * adds token and job summary
    
    * fix with
    
    * rm config
    
    * update version of action
    
    * some more configs
    
    * test another configs
    
    * delete previous comment

---
+++ test/controllers/contacts_controller_test.rb
@@ -0,0 +1,52 @@
+require "test_helper"
+
+class ContactsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get new_contact_path
+
+    assert_response :success
+  end
+
+  test "should not create contact without course params" do
+    assert_no_changes("Student.count") do
+      post contacts_url, params: {
+        contact: {
+          name: "joão",
+          email: "joao@joao.com",
+          phone: "11912345678" },
+          enrollments: {
+            course_ids: [] } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "should not create contact without email" do
+    assert_no_changes("Student.count") do
+      post contacts_url, params: {
+        contact: {
+          name: "joão",
+          email: "",
+          phone: "11912345678" },
+          enrollments: {
+            course_ids: [courses(:one).id] } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "should create contact with course params" do
+    assert_changes("Student.count") do
+      post contacts_url, params: {
+        contact: {
+          name: "joão",
+          email: "joao@joao.com",
+          phone: "11912345678" },
+          enrollments: {
+            course_ids: [courses(:one).id] } }
+    end
+    follow_redirect!
+
+    assert_response :success
+  end
+end
