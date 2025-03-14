a7711afd29f8703be118738fbe15f81eea2036ef
Josue Costa <44443163+J-Costa@users.noreply.github.com>
2025-03-13 23:26:39 -0300

    back 4 app deply test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.hosts << "krypton-nl4ye3g3.b4a.run"
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
