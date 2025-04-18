90c92142ccd8cfb3b71fbe8fdbf12ed48dc014e3
Josue Costa <44443163+J-Costa@users.noreply.github.com>
2025-04-18 20:06:36 -0300

    fix mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     authentication:       "plain",
     enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: "kryptontreinamentos.com.br" }
+  config.action_mailer.default_url_options = { host: "www.kryptontreinamentos.com.br" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
