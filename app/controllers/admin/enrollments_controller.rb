e8583e2f07b684bd7b6983bb3a0dbd51b5298236
Josue Costa <juncosta@gmail.com>
2025-08-30 12:30:13 -0300

    order by created_at

--- app/controllers/admin/enrollments_controller.rb
+++ app/controllers/admin/enrollments_controller.rb
@@ -1,5 +1,9 @@
 class Admin::EnrollmentsController < Admin::BaseController
   def index
-    @enrollments = Enrollment.group(:user_id).select("user_id, string_agg(enrollments.course_id::text, ',') as course_ids")
+    @enrollments = Enrollment
+      .joins(:user)
+      .group(:user_id, 'users.created_at')
+      .select("user_id, string_agg(enrollments.course_id::text, ',') as course_ids")
+      .order('users.created_at DESC')
   end
 end
