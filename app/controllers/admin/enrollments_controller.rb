b5f5938cc341d656b37b683b1e55b08314040a5f
Josue Costa <juncosta@gmail.com>
2025-08-30 12:31:53 -0300

    rubocoping

--- app/controllers/admin/enrollments_controller.rb
+++ app/controllers/admin/enrollments_controller.rb
@@ -2,8 +2,8 @@ class Admin::EnrollmentsController < Admin::BaseController
   def index
     @enrollments = Enrollment
       .joins(:user)
-      .group(:user_id, 'users.created_at')
+      .group(:user_id, "users.created_at")
       .select("user_id, string_agg(enrollments.course_id::text, ',') as course_ids")
-      .order('users.created_at DESC')
+      .order("users.created_at DESC")
   end
 end
