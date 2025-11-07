65f546674c2eb0e3bacd5f82802d613c5a4aff75
Josue Costa <juncosta@gmail.com>
2025-11-07 19:20:01 -0300

    Adds enrolled field to student tests adjusts on views changes fixture refactor on course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+  FEATURED_RANGE = 1..9
+
   has_many :enrollments
   has_many :students, through: :enrollments
   has_one_attached :marketing
@@ -6,5 +8,5 @@ class Course < ApplicationRecord
   validates :name, :description, :featured, presence: true
   validates :name, uniqueness: true
   validates :description, length: { minimum: 10 }
-  validates :featured, numericality: { in: 1..9 }
+  validates :featured, numericality: { in: FEATURED_RANGE }
 end
